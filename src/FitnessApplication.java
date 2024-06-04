0f138e47b16da1e03f9803c5b3de7c7d1d08a854
Oscar Puebla <pueblao@ferris.edu>
2024-06-04 18:47:16 -0400

    Tried to change image icon of each window for the app without repeating code

--- src/FitnessApplication.java
+++ src/FitnessApplication.java
@@ -3,13 +3,38 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class FitnessApplication {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-           // new FitnessApplication().run();
-        	new LoginPage().setVisible(true);
+            setApplicationIcon();
+            new LoginPage().setVisible(true);
         });
     }
 
-
+    private static void setApplicationIcon() {
+        try {
+            String iconPath = "C:\\Users\\oscar\\OneDrive\\Desktop\\Classes\\DONE\\Java Programming 2\\FitnessTrackerLogo.png"; 
+            ImageIcon icon = new ImageIcon(iconPath);
+            Image image = icon.getImage();
+            // Set the icon image for all windows
+           
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            JDialog.setDefaultLookAndFeelDecorated(true);
+            for (Window window : Window.getWindows()) {
+                if (window instanceof JFrame || window instanceof JDialog) {
+                    window.setIconImage(image);
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
+
+
+
+
+
+
+
